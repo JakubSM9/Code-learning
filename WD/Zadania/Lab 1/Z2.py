bfaaee21275331b18cbb5d2c7fd4fe8e9b02a12e
MgdlenaT <93317140+MgdlenaT@users.noreply.github.com>
2022-05-10 17:49:51 +0200

    2

--- WD/Zadania/Lab 1/Z2.py
+++ WD/Zadania/Lab 1/Z2.py
@@ -1,3 +1,11 @@
 #zad2
 
-#cos cos cos
+import math
+#4
+print(math.floor(3.55))
+print(math.ceil(4.80))
+#5
+imie='magdalena'
+nazwisko='truszkowska'
+print(imie.capitalize())
+print(nazwisko.capitalize())
