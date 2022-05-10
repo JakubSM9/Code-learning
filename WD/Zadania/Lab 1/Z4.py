df8cf96c6242698e87d9894081106c3e477dd24a
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-05-10 18:42:18 +0200

    Update Z4.py

--- WD/Zadania/Lab 1/Z4.py
+++ WD/Zadania/Lab 1/Z4.py
@@ -1,12 +1,21 @@
 import math
+from re import I
 #4
+
 #przykład1
 print(math.exp(10)) #e
+
 #przykład2
-print(5+(math.sin(8)**2))
-print(math.log(5+(math.sin(8)**2)))
-print((math.log(5+(math.sin(8)**2))**1/6))
+
+a = math.sin(8) # obliczanie sinusa
+b = 5 + (a**2) # obliczanie nawiasu
+c = math.log(b) # logarytm naturalny z nawiasu
+i = 1/6
+d = c**i # podniesienie do potęgi 1/6
+print(d) # wydrukowanie wyniku
+
 #przykład3
 print(math.floor(3.55)) #zaokrąglenie do dołu
+
 #przykład4
 print(math.ceil(4.80)) #zaokrąglenie do góry
