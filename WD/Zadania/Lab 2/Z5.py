5f62cf343ca3cb6e9a6f2fe03290eaf07857f9a0
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-05-10 19:43:54 +0200

    Update Z5.py

--- WD/Zadania/Lab 2/Z5.py
+++ WD/Zadania/Lab 2/Z5.py
@@ -1,23 +1,16 @@
-# a=input()
-# b= input()
-# c= input()
-# wynik= (a**b)+c
-# print(wynik)
-
-
 # Lab2.Z5
 # Napisz skrypt gdzie pobierzesz trzy liczby całkowite, 
 # gdzie wykonasz obliczenia: a**b + c.
 # Użyj instrukcji readline() i write()).
 
-i = input('Podaj 3 liczby calkowite, oddzielajac je spacja: ')
+i = input('Podaj 3 liczby calkowite, oddzielajac je spacja: ') # Pobieranie trzech liczb od urzytkownika 
 
-f = open("myfile.txt", "w")
+f = open("myfile.txt", "w")    # Tworzenie pliku 'myfile.txt' i wpisanie do niego trzech liczb
 f.write(i)
 
-f = open("myfile.txt", "r")
+f = open("myfile.txt", "r")             # otworzenie i przeczytanie pliku 'myfile.txt', a następnie zapisanie jego zawartosci w liscie
 a = [int(x) for x in f.read().split()]
 
-b= a[0]**a[1]+a[2]
+b= a[0]**a[1]+a[2] # Urzycie zawartości listy do rozwiązania równania
 
-print('twoj wynik to:', b)
+print('twoj wynik to:', b) # Wydrukowanie wyniku równania
